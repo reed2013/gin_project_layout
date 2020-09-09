b1814d0e980d990f16e175591382cd6f0ead1447
chenxiangping <cxp560202@163.com>
2020-09-09 08:40:31 +0800

    add tracer

--- api/routers/router.go
+++ api/routers/router.go
@@ -3,12 +3,14 @@ package routers
 import (
 	"github.com/reed2013/gin_project_layout/api/controllers"
 	"github.com/gin-gonic/gin"
+	"github.com/reed2013/gin_project_layout/api/internel/mymiddlewares"
 )
 
 func NewRouter() *gin.Engine {
 	r := gin.New()
 	r.Use(gin.Logger())
 	r.Use(gin.Recovery())
+	r.Use(mymiddlewares.Trace())
 
 	api := r.Group("")
 	{
